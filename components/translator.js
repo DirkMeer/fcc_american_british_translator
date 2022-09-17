257e3af76d9d7fdfdc80387c359a0a91b28ffee9
Dirk van Meerveld <d.v.meerveld@gmail.com>
2022-09-17 13:54:37 +0200

    unit-tests complete and double-translation bug fixed

--- components/translator.js
+++ components/translator.js
@@ -13,8 +13,8 @@ class Translator {
             //the 'i' is for case insensitive. Not perfect but the fcc example is the same
             let regex = new RegExp(`\\b${key}\\b`, 'gi')
             translationInProgress = translationInProgress.replace(
-                regex,
-                `<span class="highlight">${dictionary[key]}</span>`)
+                regex, //we add the QQQQ as temporary markers to prevent double translation of the same words
+                `<span class="highlight">QQQQ${dictionary[key]}QQQQ</span>`)
         })
         return translationInProgress
     }
@@ -28,8 +28,8 @@ class Translator {
             //the 'i' is for case insensitive. Not perfect but the fcc example is the same
             let regex = new RegExp(`\\b${dictionary[key]}\\b`, 'gi')
             translationInProgress = translationInProgress.replace(
-                regex,
-                `<span class="highlight">${key}</span>`)
+                regex, //we add the QQQQ as temporary markers to prevent double translation of the same words
+                `<span class="highlight">QQQQ${key}QQQQ</span>`)
         })
         return translationInProgress
     }
@@ -41,13 +41,35 @@ class Translator {
             //we create a safe key. The first regex looks for a dot (ironically has to be escaped by a \.)
             //the second regex replaces the . with a \. which again has to be escaped by a \ to make \\.
             let safeKey = key.replace(/\./, '\\.')
-            console.log(safeKey)
+            //console.log(safeKey)
             //input the safe key and remove word boundary at the end! (or the dot will create a word boundary early)
             let regex = new RegExp(`\\b${safeKey}`, 'gi')
             translationInProgress = translationInProgress.replace(
-                regex,
-                `<span class="highlight">${dictionary[key]}</span>`)
+                regex, //we add the QQQQ as temporary markers to prevent double translation of the same words
+                `<span class="highlight">QQQQ${dictionary[key]}QQQQ</span>`)
         })
+        return translationInProgress
+    }
+
+    //convert time notation A to B
+    timeAmericanToBritish(translationInProgress) {
+        let timeRegex = new RegExp(/\b([0-9]{1,2})\:([0-6][0-9])\b/g)
+        translationInProgress = translationInProgress.replace(
+            timeRegex,
+            //note how we input regex capture groups without even using ${varname}
+            `<span class="highlight">$1.$2</span>`
+        )
+        return translationInProgress
+    }
+
+    //convert time notation B to A
+    timeBritishToAmerican(translationInProgress) {
+        let timeRegex = new RegExp(/\b([0-9]{1,2})\.([0-6][0-9])\b/g)
+        translationInProgress = translationInProgress.replace(
+            timeRegex,
+            //note how we input regex capture groups without even using ${varname}
+            `<span class="highlight">$1:$2</span>`
+        )
         return translationInProgress
     }
 
@@ -55,25 +77,38 @@ class Translator {
     translate(text, mode){
         let translationInProgress = text
         if(mode === 'american-to-british'){
-            //translate our American only words
-            translationInProgress = this.useDictLeftRight(americanOnly, translationInProgress)
             //translate our words from American to British
             translationInProgress = this.useDictLeftRight(americanToBritishSpelling, translationInProgress)
             //translate our titles from American to British
             translationInProgress = this.useAmericanToBritishTitles(americanToBritishTitles, translationInProgress)
+            //translate our time notation from American to British
+            translationInProgress = this.timeAmericanToBritish(translationInProgress)
+            //translate our American only words
+            translationInProgress = this.useDictLeftRight(americanOnly, translationInProgress)
         
         } else if(mode === 'british-to-american') {
-            //translate our British only words
-            translationInProgress = this.useDictLeftRight(britishOnly, translationInProgress)
             //translate our words from British to American
             translationInProgress = this.useDictReverse(americanToBritishSpelling, translationInProgress)
             //translate our titles from British to American
             translationInProgress = this.useDictReverse(americanToBritishTitles, translationInProgress)
+            //translate our time notation from British to American
+            translationInProgress = this.timeBritishToAmerican(translationInProgress)
+            //translate our British only words
+            translationInProgress = this.useDictLeftRight(britishOnly, translationInProgress)
+        }
+
+        //test if anything was translated at all//
+        let translation
+        if(text === translationInProgress) {
+            translation = 'Everything looks good to me!'
+        } else { //remove the temporary double-translation-prevention markers
+            translation = translationInProgress.replaceAll('QQQQ', '')
+            //Note we have to use replaceAll because replace would only work for 1 occurrence
         }
 
         let returnObject = {
             text: text,
-            translation: translationInProgress
+            translation: translation
         }
         return returnObject
     }
